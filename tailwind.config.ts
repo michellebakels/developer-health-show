df2efa55c1a7b6edc248977275d17f9a55a5bff3
michellebakels <michelle.bakels@gmail.com>
2023-11-09 15:41:42 -0500

    add basement foundry typeface

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        blob: ['var(--font-blob)'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
